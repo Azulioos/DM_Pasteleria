8a24a3685c7c67705639e4bca51e7af05b141bc3
sjas6 <numerocuarentayuno@gmail.com>
2021-04-18 22:00:25 -0500

    Agregada acción al menu, la acción de logout y el cambiar idioma.

--- app/src/main/java/com/example/dmpasteleria/Pe_admin_inicio.java
+++ app/src/main/java/com/example/dmpasteleria/Pe_admin_inicio.java
@@ -4,17 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Pe_admin_inicio extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pe_admin_inicio);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar_i);
         setSupportActionBar(toolbar);
@@ -56,5 +61,13 @@ public class Pe_admin_inicio extends AppCompatActivity {
         inflater.inflate(R.menu.amenu,menu);
         return true;
 
+    }
+
+    public void logout(MenuItem item) {
+        FirebaseAuth fAuth;
+        fAuth = FirebaseAuth.getInstance();
+        fAuth.signOut();
+        startActivity(new Intent(getApplicationContext(), Pe_inicio.class));
+        finish();
     }
 }

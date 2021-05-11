8b85534f4954f04dc42883a331141501baff956d
Azulioos <ldgarcialeal@hotmail.com>
2021-05-10 23:28:16 -0500

    Initial Commit

--- app/src/main/java/com/example/dmpasteleria/Pe_usuario_pedidos.java
+++ app/src/main/java/com/example/dmpasteleria/Pe_usuario_pedidos.java
@@ -1,30 +1,37 @@
 package com.example.dmpasteleria;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-
-import java.util.ArrayList;
 
 public class Pe_usuario_pedidos extends AppCompatActivity {
     private FirebaseFirestore fstore;
     private RecyclerView recyclerView;
-    private FirestoreRecyclerAdapter adapter;
+    FirebaseUser Usuario;
+    String Usuario_2;
+    FirebaseAuth fAuth;
 
-    Pe_usuario_pedidos_adapter myAdapter;
-    ArrayList<Pe_usuario_pedidos_datos> list;
+    FirestoreRecyclerAdapter adapter;
+
     private static final String TAG = "Pedidos";
 
 
@@ -32,51 +39,59 @@ public class Pe_usuario_pedidos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pe_usuario_pedidos);
+        Toolbar toolbar = findViewById(R.id.toolbar_i);
+        setSupportActionBar(toolbar);
 
+        fAuth = FirebaseAuth.getInstance();
         fstore = FirebaseFirestore.getInstance();
         recyclerView = findViewById(R.id.userList);
+        Usuario = FirebaseAuth.getInstance().getCurrentUser();
+        Usuario_2 = Usuario.getUid();
 
-        Query query = fstore.collection("pedidos");
+        Query query = fstore.collection("pedidos").
+                whereEqualTo("Usuario", Usuario_2);
         FirestoreRecyclerOptions<Pe_usuario_pedidos_datos> options = new FirestoreRecyclerOptions.Builder<Pe_usuario_pedidos_datos>()
                 .setQuery(query, Pe_usuario_pedidos_datos.class)
                 .build();
 
-         adapter = new FirestoreRecyclerAdapter<Pe_usuario_pedidos_datos, ProductViewHolder>(options) {
+         adapter = new FirestoreRecyclerAdapter<Pe_usuario_pedidos_datos, ProductsViewHolder>(options) {
             @NonNull
             @Override
-            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            public ProductsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pe_usuario_pedidos_objetos, parent, false);
-                return new ProductViewHolder(view);
+                return new ProductsViewHolder(view);
             }
 
             @Override
-            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Pe_usuario_pedidos_datos model) {
-                holder.list_tamano.setText(model.getTamano());
-                holder.list_pan.setText(model.getPan());
-                holder.list_condimentos.setText(model.getCondimentos());
-                holder.list_colores.setText(model.getColores());
-                holder.list_extra.setText(model.getExtra());
+            protected void onBindViewHolder(@NonNull ProductsViewHolder holder, int position, @NonNull Pe_usuario_pedidos_datos model) {
+                holder.tvTamano.setText(model.getTamano());
+                holder.tvPan.setText(model.getPan());
+                holder.tvCondimentos.setText(model.getCondimentos());
+                holder.tvColores.setText(model.getColores());
+                holder.tvExtra.setText(model.getExtra());
             }
         };
-        recyclerView.setHasFixedSize(true);
+        recyclerView.setHasFixedSize(false);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
 
-    private class ProductViewHolder extends RecyclerView.ViewHolder{
-        private TextView list_tamano;
-        private  TextView list_pan;
-        private TextView list_condimentos;
-        private  TextView list_colores;
-        private TextView list_extra;
+    private class ProductsViewHolder extends RecyclerView.ViewHolder{
 
-        public ProductViewHolder(@NonNull View itemView){
+        private TextView tvTamano;
+        private  TextView tvPan;
+        private TextView tvCondimentos;
+        private  TextView tvColores;
+        private TextView tvExtra;
+
+
+        public ProductsViewHolder(@NonNull View itemView) {
             super(itemView);
-            list_tamano = itemView.findViewById(R.id.tvTamano);
-            list_pan = itemView.findViewById(R.id.tvPan);
-            list_condimentos = itemView.findViewById(R.id.tvCondimentos);
-            list_colores = itemView.findViewById(R.id.tvColores);
-            list_extra = itemView.findViewById(R.id.tvExtra);
+            tvTamano = itemView.findViewById(R.id.tvTamano);
+            tvPan = itemView.findViewById(R.id.tvPan);
+            tvCondimentos = itemView.findViewById(R.id.tvCondimentos);
+            tvColores = itemView.findViewById(R.id.tvColores);
+            tvExtra = itemView.findViewById(R.id.tvExtra);
         }
     }
 
@@ -90,5 +105,35 @@ public class Pe_usuario_pedidos extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         adapter.startListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+
+    }
+
+    public void signOut(MenuItem item) {
+        FirebaseAuth fAuth;
+        fAuth = FirebaseAuth.getInstance();
+        fAuth.signOut();
+        startActivity(new Intent(getApplicationContext(), Pe_inicio.class));
+        finish();
+    }
+
+
+    public void openUserAccount(MenuItem item) {
+        FirebaseAuth fAuth;
+        fAuth = FirebaseAuth.getInstance();
+        fAuth.signOut();
+        startActivity(new Intent(getApplicationContext(), Pe_user_settings.class));
+        finish();
+    }
+
+    public void Personalized(View view) {
+        startActivity(new Intent(getApplicationContext(), Pe_usuario_pasteles_personalizados.class));
+        finish();
     }
 }
